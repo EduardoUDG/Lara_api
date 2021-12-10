bb7aac75f1181fd6a5cce0bafd0719e680d9dc5d
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2021-12-10 14:31:22 -0600

    update - user register and postman

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -3,10 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\RegisterController;
 
-Route::get('/prueba', function() {
-    return "prueba";
-});
+Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
